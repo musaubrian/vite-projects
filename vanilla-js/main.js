8a70919d9a2c249efdde54254aa7f6e6bf12f79b
Ernest_musau <musaubrian45@gmail.com>
2022-01-23 15:55:09 +0300

    Added the vanilla js folder

---
+++ vanilla-js/main.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+const content = `
+  <h1>Hello Vite!</h1>
+  <button>
+    Vanilla JS
+  </button>
+  
+  <div>
+    <h4>I am an H4 </h4>
+  </div>
+`
+document.querySelector('#app').innerHTML = content 
