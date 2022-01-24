1e1f797e5c176ab67c565cfc35176b21056816ef
Ernest_musau <musaubrian45@gmail.com>
2022-01-24 16:53:02 +0300

    filled in the html template

--- vanilla-ts/src/main.ts
+++ vanilla-ts/src/main.ts
@@ -2,7 +2,39 @@ import './style.css'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+const htmlInject = `
+
+  <div class="wrapper">
+    <h1>Finance Logger</h1>
+
+    <ul class="item list">
+    
+    </ul>
+  
+  </div>
+
+  <footer>
+    <form class="new-item-form">
+      <label for="type">Type: </label>
+      <select id="type" class="inputs">
+        <option value="invoice"> Invoice </option>
+        <option value="payment"> Payment </option>
+      </select>
+
+
+
+      <label for="tofrom"> To/From: </label>
+      <input type="text" name="tofrom" placeholder="Name" class="inputs"/>
+
+      <label for="details"> Details: </label>
+      <input type="text" name="details" placeholder=" Details" class="inputs"/>
+
+      <label for="amount"> Amount ($):</label>
+      <input type="text" name="amount" placeholder=" $ " class="inputs"/>
+      <button> Add </button>
+      
+    </form>
+  </footer>
 `
+app.innerHTML = htmlInject
+
