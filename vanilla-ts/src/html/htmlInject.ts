c214d788d97e201e3ac6c4bebcce8406dd367e79
Ernest_musau <musaubrian45@gmail.com>
2022-02-09 17:41:38 +0300

    implemented the render function in the main.ts

---
+++ vanilla-ts/src/html/htmlInject.ts
@@ -0,0 +1,40 @@
+export const htmlInject = `
+
+  <div class="wrapper">
+    <h1>Finance Logger</h1>
+
+    <ul class="item-list">
+    
+    </ul>
+  
+  </div>
+  <a class="waves-effect waves-dark btn modal-trigger" href="#modal">
+    <span class="material-icons">
+        add
+    </span>
+  </a>
+
+  <div class="modal" id="modal">
+    <form class="new-item-form">
+      <div class="input-field">
+        <select id="type" class="inputs select">
+          <option value="" disabled selected>Choose your option</option>
+          <option value="invoice">Invoice</option>
+          <option value="payment">Payment</option>
+        </select>
+        <label>Type</label>
+      </div>      
+
+      <label for="tofrom"> To/From: </label>
+      <input type="text" name="tofrom" placeholder="Name" class="inputs" id="tofrom"/>
+
+      <label for="details"> Details: </label>
+      <input type="text" name="details" placeholder=" Details" class="inputs" id="details"/>
+
+      <label for="amount"> Amount ($):</label>
+      <input type="text" name="amount" class="inputs" id="amount"/>
+      <button class="submit"> Add </button>
+
+    </form>
+  </div>
+`;
--- vanilla-ts/src/main.ts
+++ vanilla-ts/src/main.ts
@@ -3,50 +3,11 @@ import { Invoice } from './classes/invoice'
 import { Payment } from './classes/payment'
 import { HasFormatter } from './interface/hasFormatter'
 import { ListTemplate } from './classes/listTemplate'
+import { htmlInject } from './html/htmlInject'
 
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
-const htmlInject = `
-
-  <div class="wrapper">
-    <h1>Finance Logger</h1>
-
-    <ul class="item-list">
-    
-    </ul>
-  
-  </div>
-  <a class="waves-effect waves-dark btn modal-trigger" href="#modal">
-    <span class="material-icons">
-        add
-    </span>
-  </a>
-
-  <div class="modal" id="modal">
-    <form class="new-item-form">
-      <div class="input-field">
-        <select id="type" class="inputs select">
-          <option value="" disabled selected>Choose your option</option>
-          <option value="invoice">Invoice</option>
-          <option value="payment">Payment</option>
-        </select>
-        <label>Type</label>
-      </div>      
-
-      <label for="tofrom"> To/From: </label>
-      <input type="text" name="tofrom" placeholder="Name" class="inputs" id="tofrom"/>
-
-      <label for="details"> Details: </label>
-      <input type="text" name="details" placeholder=" Details" class="inputs" id="details"/>
-
-      <label for="amount"> Amount ($):</label>
-      <input type="text" name="amount" class="inputs" id="amount"/>
-      <button class="submit"> Add </button>
-
-    </form>
-  </div>
-`
 app.innerHTML = htmlInject
 
 
@@ -71,7 +32,7 @@ form.addEventListener('submit', (e: Event) => {
     doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
 
-  console.log(doc);
+  list.render(doc, tofrom.value, "start");
   
 })
 
