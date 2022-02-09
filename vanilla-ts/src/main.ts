c6205eae1acf790518d71305b4996569811abcc4
Ernest_musau <musaubrian45@gmail.com>
2022-02-09 17:37:45 +0300

    did the list template class

--- vanilla-ts/src/main.ts
+++ vanilla-ts/src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 import { Invoice } from './classes/invoice'
 import { Payment } from './classes/payment'
 import { HasFormatter } from './interface/hasFormatter'
+import { ListTemplate } from './classes/listTemplate'
 
 
 const app = document.querySelector<HTMLDivElement>('#app')!
@@ -55,6 +56,9 @@ const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
 
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
